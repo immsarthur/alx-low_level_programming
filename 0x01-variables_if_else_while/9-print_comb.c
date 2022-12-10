d8ea8be9ade2ea77bf6037cfef27d88b71b82df0
immsarthur <immsarthur@gmail.com>
2022-12-09 23:04:14 -0800

    combinations of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all the numbers of base 16 in lowercase.
+ * followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
